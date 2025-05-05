18a6030b6394ebc812bc8a01820366b924beeb73
Slavica <slavicamiladinova8@email.com>
2025-05-05 15:40:36 +0200

    Added new folder to the repository

---
+++ Project2_KanBanBoard/Project2_KanBanBoard/KanBanBoard/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace KanbanBoardApp
+{
+    public class Task
+    {
+        public string TaskId { get; set; }
+        public string TaskDescription { get; set; }
+
+        public Task(string taskId, string taskDescription)
+        {
+            TaskId = taskId;
+            TaskDescription = taskDescription;
+        }
+    }
+}
